5e16f6318df8ece93c27365ba989d7b501f862db
cheun0327 <48849608+cheun0327@users.noreply.github.com>
2020-01-11 14:40:14 +0900

    Update 2번.py

--- 2주차 토요일 과제/2번.py
+++ 2주차 토요일 과제/2번.py
@@ -1,15 +1,11 @@
 num=int(input())
-dp=[]
-dp.append(0)
-dp.append(0)
+dp=[0,0]
 for i in range(2,num+1):
     minn = dp[i-1]
-    if i%2==0:
-        if minn>dp[i//2]:
-            minn=dp[i//2]
-    if i%3==0:
-        if minn>dp[i//3]:
-            minn=dp[i//3]
+    if i%2==0 and minn>dp[i//2]:
+         minn=dp[i//2]
+    if i%3==0 and minn>dp[i//3]:
+         minn=dp[i//3]
     dp.append(minn+1)
 print(dp[num])
 
